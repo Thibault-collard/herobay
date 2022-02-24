85b3d82739dcd81809b92417340cb0c06b1f6972
Thibault Collard-bovy <thibault.collard@gmail.com>
2022-02-24 16:11:26 +0100

    [BUG] Problem dependencies

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -74,6 +74,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'DatabaseName',
+        'USER': 'DatabaseUserName',
+        'PASSWORD': 'DatabaseUserpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
