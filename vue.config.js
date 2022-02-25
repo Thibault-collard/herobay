e0c579f17e16e0512114aec2132ab3d444116062
Thibault Collard-bovy <thibault.collard@gmail.com>
2022-02-25 20:42:39 +0100

    update

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,6 @@ module.exports = {
     outputDir: 'dist',
     assetsDir: 'static',
     publicPath: '/',
-    // For Production, replace set baseUrl to CDN
-    // And set the CDN origin to `yourdomain.com/static`
-    // Whitenoise will serve once to CDN which will then cache
-    // and distribute
     devServer: {
       proxy: {
         '/api*': {
