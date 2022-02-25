c0e0de9abad21d741880f2dcecafc0eeaa23d79e
Thibault Collard-bovy <thibault.collard@gmail.com>
2022-02-25 16:59:25 +0100

    [FEATURES] Connect MongoDB to Heroku in prod environment

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,8 @@
-const IS_PRODUCTION = process.env.NODE_ENV === 'production'
 
 module.exports = {
     outputDir: 'dist',
     assetsDir: 'static',
-    publicPath: IS_PRODUCTION ? 'herobay.herokuapp.com': '/',
+    publicPath: '/',
     // For Production, replace set baseUrl to CDN
     // And set the CDN origin to `yourdomain.com/static`
     // Whitenoise will serve once to CDN which will then cache
