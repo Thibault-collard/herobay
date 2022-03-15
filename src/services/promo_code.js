1ba67220a46dcbf0bf3e624f5046e025da063084
Thibault-collard <thibault.collard@gmail.com>
2022-03-15 18:26:34 +0100

    [BUG] one item display the price of two items

--- src/services/promo_code.js
+++ src/services/promo_code.js
@@ -1,15 +1,18 @@
 function get1offer(state,total){
     let list_price = [];
 
-    if(state.cartCount > 0){
+    if(state.cartCount > 2){
         let countgift = Math.floor(state.cartCount / 3)
+        console.log(countgift)
         for (let item of state.cart) {
             list_price.push(Array(item.quantity).fill(Number(item.price)))
         }
         
         list_price = [].concat.apply([], list_price);
-        
+        console.log(list_price)
         const reducer = (accumulator, curr) => accumulator + curr;
+
+
         list_price = list_price.sort(function(a,b) { return parseFloat(a) - parseFloat(b) } )
         
         .slice(0, countgift).reduce(reducer)
